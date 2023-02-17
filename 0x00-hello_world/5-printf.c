c162244a17d15a40d334c802fa7f93e6fc3628b0
ihayford <hayford.isaac@gmail.com>
2023-02-16 16:28:14 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,9 +3,8 @@
  * main - using function printf to print a line
  * Return: 0 (Success)
  */
-
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n")
+printf("with proper grammar, but the outcome is a piece of art,\n")
 return (0);
 }
