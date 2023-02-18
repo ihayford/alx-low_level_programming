cce779daddca507a684595ffd93834ad5aba512a
ihayford <hayford.isaac@gmail.com>
2023-02-18 10:08:31 -0800

    prints the alphabet in lowercase, and then in uppercase

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Commands
+ * Return: 0
+ */
+
+int main(void)
+{
+	char bott;
+	for (bott = 'a'; bott <= 'z'; bott++)
+		putchar(bott);
+
+	for (bott = 'A'; bott <= 'Z'; bott++)
+		putchar(bott);
+	putchar('\n');
+
+	return (0);
+}
