442f54564c36c918238fbbb119512f4cce09d924
ihayford <hayford.isaac@gmail.com>
2023-02-22 00:39:58 -0800

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints alpha 10 times
+ */
+void print_alphabet_x10(void)
+{
+	int ten;
+
+	char alp;
+
+	for (ten = 0; ten <= 9; ten++)
+	{
+		for (alp = 'a'; alp <= 'z'; alp++)
+			_putchar(alp);
+		putchar('\n');
+	}
+}
