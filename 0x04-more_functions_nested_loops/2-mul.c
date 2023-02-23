2be238c951ea120b8208b1a408b8e1a5b677078d
ihayford <hayford.isaac@gmail.com>
2023-02-22 23:59:53 -0800

    Tasks 0 to 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * mul - will multiply two integers
+ * @a: Integer one
+ * @b: integer two
+ * Return: sum of the product
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
