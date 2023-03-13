b515e2431dd8b6af6ead903e4be237de053d6c39
ihayford <hayford.isaac@gmail.com>
2023-03-13 03:02:52 -0700

    Task 1

--- 0x09-static_libraries/4-strpbrk.c
+++ 0x09-static_libraries/4-strpbrk.c
@@ -16,12 +16,10 @@ char *_strpbrk(char *s, char *accept)
 		for (k = 0; accept[k]; k++)
 		{
 			if (*s == accept[k])
-			{
-				if (*s == accept[k])
+			
 					return (s);
 			}
 			s++;
 		}
 		return ('\0');
-	}
 }
