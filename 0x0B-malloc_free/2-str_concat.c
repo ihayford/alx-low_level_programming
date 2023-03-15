d81cf0e15a7b8aa74a8b2bf683d76c15a385b29d
ihayford <hayford.isaac@gmail.com>
2023-03-15 01:59:04 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -16,10 +16,14 @@ char *str_concat(char *s1, char *s2)
 
 	if (s1 == NULL)
 		s1 = "";
+
 	if (s2 == NULL)
 		s2 = "";
+
 	while (s1[i])
 		i++;
+	while (s2[j])
+		j++;
 
 	l = i + j;
 	s = malloc((sizeof(char) * l) + 1);
